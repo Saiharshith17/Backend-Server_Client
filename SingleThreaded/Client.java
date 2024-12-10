b774896deebf10ecabcf714cb847be8ad8de5b16
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-12-10 21:02:13 +0530

    finished upto SinglThreaded

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -9,24 +9,28 @@ import java.io.IOException;
 import java.io.BufferedReader;
 
 public class Client {
-    public void run() throws UnknownHostException,IOException{
-        int port =8010;
-        InetAddress address=InetAddress.getByName("localhost");
-        Socket socket = new Socket(address,port);
-        PrintWriter toSocket= new PrintWriter(socket.getOutputStream());
-        BufferedReader fromSocket =new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        toSocket.println("hello from the client");
-        String line=fromSocket.readLine();
-        System.out.println("Response from the socket is : "+line);
-    }
+    public void run() throws UnknownHostException, IOException {
+        int port = 8010;
+        InetAddress address = InetAddress.getByName("localhost");
+        try (Socket socket = new Socket(address, port);
+             PrintWriter toSocket = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader fromSocket = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
 
+            toSocket.println("hello from the client");
+            String line = fromSocket.readLine();
+            System.out.println("Response from the socket is : " + line);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
-        try{
-            Client client=new Client();
+        try {
+            Client client = new Client();
             client.run();
-        }catch(Exception e){
-         e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-}
 }
--- SingleThreaded/Server.java
+++ SingleThreaded/Server.java
@@ -1,9 +1,7 @@
 package SingleThreaded;
-import java.io.IOException;
+
 import java.net.ServerSocket;
-import java.io.BufferedReader;
 import java.io.*;
-import java.util.*;
 import java.net.Socket;
 
 public class Server {
@@ -18,14 +16,16 @@ public class Server {
                  BufferedReader fromClient = new BufferedReader(new InputStreamReader(acceptedConnection.getInputStream()))) {
 
                 System.out.println("Connection accepted from Client " + acceptedConnection.getRemoteSocketAddress());
-
-                // Send a message to the client
                 toClient.println("Hello from the server");
                 System.out.println("Message sent to client: Hello from the server");
 
-                // Read the client's message
                 String clientMessage = fromClient.readLine();
-                System.out.println("Client says: " + clientMessage);
+                if (clientMessage != null) {
+                    System.out.println("Client says: " + clientMessage);
+                } else {
+                    System.out.println("Client disconnected");
+                }
+
             } catch (IOException e) {
                 System.err.println("Error handling client connection: " + e.getMessage());
             }
