70d6587680601880fadd822b9ac646a739bfd811
Saiharshith Eedara <142712608+Saiharshith17@users.noreply.github.com>
2025-03-01 00:17:06 +0530

    Update Client.java

--- SingleThreaded/Client.java
+++ SingleThreaded/Client.java
@@ -8,6 +8,8 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.BufferedReader;
 
+
+
 public class Client {
     public void run() throws UnknownHostException, IOException {
         int port = 8010;
